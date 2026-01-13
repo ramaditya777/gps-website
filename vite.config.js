492241c8981ffe178df3b9d6cbd56d7ab8fe9d55
Ramaditya panda <ramadityapanda3@gmail.com>
2026-01-13 23:17:25 +0530

    change in vit cofig file

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,4 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/gps-website/", // repo name
-  build: {
-    outDir: "dist",
-  },
 });
