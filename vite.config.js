c414f76e3bed6f46f9e586c792dc2c70e14b2179
Ramaditya panda <ramadityapanda3@gmail.com>
2026-01-13 20:43:21 +0530

    Fix Vite Vercel CSS paths

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/gps-website/", // repo name
+  base: "/", // repo name
+  build: {
+    outDir: "dist",
+  },
 });
