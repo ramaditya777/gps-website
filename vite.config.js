1a87d3cbfec7a22610ff52506ef3967f1e26d14d
Ramaditya panda <ramadityapanda3@gmail.com>
2026-01-13 20:50:44 +0530

    hosting using git

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", // repo name
+  base: "/gps-website/", // repo name
   build: {
     outDir: "dist",
   },
